8ed65252c0c3e6a3eacdf7e2b530a7a463d151d1
Barbie <barbie@missbarbell.co.uk>
2014-06-08 17:26:15 +0100

    updated copyright date

--- lib/Parse/CPAN/Distributions.pm
+++ lib/Parse/CPAN/Distributions.pm
@@ -298,7 +298,7 @@ L<Parse-CPAN-Packages>
 
 =head1 COPYRIGHT AND LICENSE
 
-  Copyright (C) 2008-2013 Barbie for Miss Barbell Productions.
+  Copyright (C) 2008-2014 Barbie for Miss Barbell Productions.
 
 This distribution is free software; you can redistribute it and/or
 modify it under the Artistic License v2.
